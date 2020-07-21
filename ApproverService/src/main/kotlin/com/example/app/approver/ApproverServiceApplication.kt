af1ec42031bb292e85f61572bff585f46488a5f5
Nikolay Mikhaylov <hardsky@yandex.ru>
2020-07-21 21:03:21 +0300

    ApproverService skeleton project

---
+++ ApproverService/src/main/kotlin/com/example/app/approver/ApproverServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.app.approver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApproverServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApproverServiceApplication>(*args)
+}
