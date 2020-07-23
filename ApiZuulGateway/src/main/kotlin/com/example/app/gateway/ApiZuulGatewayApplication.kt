8f31f14578f21c3a98a343cb42a8270462df863e
Nikolay Mikhaylov <hardsky@yandex.ru>
2020-07-23 18:26:56 +0300

    dockerize application (start)

--- ApiZuulGateway/src/main/kotlin/com/example/app/gateway/ApiZuulGatewayApplication.kt
+++ ApiZuulGateway/src/main/kotlin/com/example/app/gateway/ApiZuulGatewayApplication.kt
@@ -2,8 +2,10 @@ package com.example.app.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
+@EnableZuulProxy
 class ApiZuulGatewayApplication
 
 fun main(args: Array<String>) {
